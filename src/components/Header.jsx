f395704922936cdb3f0f7dca75217634296651b5
LizTH09 <sele99na@gmail.com>
2022-09-16 01:15:42 -0500

    cambios en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import './header.css'
 
 const Header = ({ setValueSearch }) => {
   const [inputValue, setInputValue] = useState("")
@@ -9,13 +10,14 @@ const Header = ({ setValueSearch }) => {
   return (
     <header className="header">
       <h1 className="header__title">Título</h1>
-      <form onSubmit={onSubmitValue}>
-        <input
-          className="header__search"
-          type="text"
-          placeholder='Buscar gifs'
-          onChange={(e) => setInputValue(e.target.value)}
-        />
+      <form className='header__form' onSubmit={onSubmitValue}>
+          <input
+            className="header__search"
+            type="text"
+            placeholder='Buscar gifs'
+            onChange={(e) => setInputValue(e.target.value)}
+            autoComplete = "off"
+          />
       </form>
     </header>
   );
