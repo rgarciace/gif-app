4952608a3cb4a607cdc84a8985d8a8c07f9b9d70
Renzo Samuel García Cervera <renzogc745@gmail.com>
2022-09-18 12:37:55 -0500

    Mejoras visuales de la grilla

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,49 +23,83 @@ const Header = ({ setValueSearch }) => {
   return (
     <div className="containerHeader">
       <header className="header">
-        <div className="header__main">
-          <img className="header__main--logo" src={Logo} alt="Logotipo" />
-          <form className="header__main--form" onSubmit={onSubmitValue}>
-            <input
-              className="form__search"
-              type="text"
-              placeholder="Buscar gifs"
-              onChange={(e) => setInputValue(e.target.value)}
-              autoComplete="off"
-            />
-            <Search className="form__icon" />
-          </form>
-        </div>
-        <div className="header__options">
-          <button
-            className={
-              seeTrends
-                ? "header__options--option header__options--option-see"
-                : "header__options--option"
-            }
-            onClick={onClickTrends}
-          >
-            Tendencias
-          </button>
-          <button
-            className={
-              seeCategories
-                ? "header__options--option header__options--option-see"
-                : "header__options--option"
-            }
-            onClick={onClickCategories}
-          >
-            Categorías
-          </button>
-        </div>
-        {seeTrends && (
-          <div className="header__trends">Aquí mostrar lista de tendencias</div>
-        )}
-        {seeCategories && (
-          <div className="header__categories">
-            Aquí mostrar lista de categorías
+        <div className="header__content-header">
+          <div className="header__main">
+            <img className="header__main--logo" src={Logo} alt="Logotipo" />
+            <form className="header__main--form" onSubmit={onSubmitValue}>
+              <input
+                className="form__search"
+                type="text"
+                placeholder="Buscar gifs"
+                onChange={(e) => setInputValue(e.target.value)}
+                autoComplete="off"
+              />
+              <Search className="form__icon" />
+            </form>
           </div>
-        )}
+          <div className="header__options">
+            <button
+              className={
+                seeTrends
+                  ? "header__options--option header__options--option-see"
+                  : "header__options--option"
+              }
+              onClick={onClickTrends}
+            >
+              Tendencias
+            </button>
+            <button
+              className={
+                seeCategories
+                  ? "header__options--option header__options--option-see"
+                  : "header__options--option"
+              }
+              onClick={onClickCategories}
+            >
+              Categorías
+            </button>
+          </div>
+        </div>
+        <div className="header__sub-header">
+          {seeTrends && (
+            <div className="header__trends">
+              <button className={`trends__trend trends__trend--color-${Math.floor(Math.random() * 5) + 1}`}>
+                Hello world
+              </button>
+              <button className={`trends__trend trends__trend--color-${Math.floor(Math.random() * 5) + 1}`}>
+                Hello world
+              </button>
+              <button className={`trends__trend trends__trend--color-${Math.floor(Math.random() * 5) + 1}`}>
+                Hello world
+              </button>
+              <button className={`trends__trend trends__trend--color-${Math.floor(Math.random() * 5) + 1}`}>
+                Hello world
+              </button>
+              <button className={`trends__trend trends__trend--color-${Math.floor(Math.random() * 5) + 1}`}>
+                Hello world
+              </button>
+            </div>
+          )}
+          {seeCategories && (
+            <div className="header__categories">
+              <button className={`trends__trend trends__trend--color-${Math.floor(Math.random() * 5) + 1}`}>
+                Hola mundo
+              </button>
+              <button className={`trends__trend trends__trend--color-${Math.floor(Math.random() * 5) + 1}`}>
+                Hola mundo
+              </button>
+              <button className={`trends__trend trends__trend--color-${Math.floor(Math.random() * 5) + 1}`}>
+                Hola mundo
+              </button>
+              <button className={`trends__trend trends__trend--color-${Math.floor(Math.random() * 5) + 1}`}>
+                Hola mundo
+              </button>
+              <button className={`trends__trend trends__trend--color-${Math.floor(Math.random() * 5) + 1}`}>
+                Hola mundo
+              </button>
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
