9de6861aaf7271bfae4ab9f83d452f92d2a851e5
Renzo Samuel García Cervera <renzogc745@gmail.com>
2022-09-18 20:34:15 -0500

    Página de inicio con tendencias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,34 +4,35 @@ import GifResults from "./components/GifResults";
 import Header from "./components/Header";
 import "./giftExpertApp.css";
 
-const url = "https://api.giphy.com/v1/gifs/search";
 const api = "edPUisklOkr7bKicTfrJR1shTmyurGWr";
 const limit = 20;
 
 const GifExpertApp = () => {
   const [valueSearch, setValueSearch] = useState("");
   const [results, setResults] = useState([]);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     getGif();
   }, [valueSearch]);
 
   const getGif = async () => {
-    const response = await fetch(
-      `${url}?api_key=${api}&q=${valueSearch}&limit=${limit}`
-    );
+    const url =
+      valueSearch !== ""
+        ? `https://api.giphy.com/v1/gifs/search?api_key=${api}&limit=${limit}&q=${valueSearch}`
+        : `https://api.giphy.com/v1/gifs/trending?api_key=${api}&limit=${limit}`;
+    const response = await fetch(url);
     const { data } = await response.json();
     setResults(data);
-    setLoading(false)
+    setLoading(false);
   };
 
   return (
     <div className="app">
       <div className="app__container">
-        <Header setValueSearch={setValueSearch}/>
-        <GifResults results={results} loading={loading}/>
+        <Header setValueSearch={setValueSearch} />
+        <GifResults results={results} loading={loading} />
         <Footer></Footer>
       </div>
     </div>
